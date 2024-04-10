39cd7ea7218bb22ec9c98300228f7b86309fe371
GB-Navarro <gbnavarro@usp.br>
2024-04-10 07:59:45 -0300

    add: created the main methods of the class 'Graph'

--- consensus.cpp
+++ consensus.cpp
@@ -1,11 +1,16 @@
+// TO-DO
+//  1 - Comentar o código
+//  2 - Ajustar o operador '=' na classe Node, (tá dando problema com a constante NUMBER)
+
 /*              Bibliotecas que serão usadas no código              */
 
 // Biblioteca que contém o tipo 'uint8_t'.
 #include <cstdint>
 // Biblioteca que contém o objeto 'vector'.
 #include <vector>
-// Biblioteca responsável pela entrada e saída.
+// Bibliotecas responsáveis pela entrada e saída.
 #include <stdio.h>
+#include <iostream>
 
 /*              Structs que serão usadas no código              */
 
@@ -38,6 +43,7 @@ const Color Configs::OFF = {0, 0, 0};
 
 
 /*              Classes que serão usadas no código              */
+
 class Node{
     /*
         Descrição:
@@ -45,9 +51,9 @@ class Node{
         
         Atributos:
             public:
-                NUMBER (const uint8_t): Constante numérica inteira que representa o número do nó.
+                number (uint8_t):  número inteiro que representa o nó.
                 color (Color): Struct do tipo 'Color' que representa a cor atual que o nó possui.
-                neighbors (vector<uint8_t>): Lista de inteiros que indica quais são os vizinhos do nó.
+                neighbors (vector<uint8_t>): Array de inteiros que indica quais são os vizinhos do nó.
             private:
                 neighbors_count (uint8_t): Valor númerico que representa a quantidade de vizinhos do nó.
 
@@ -64,16 +70,16 @@ class Node{
     */
 
     public:
-        const uint8_t NUMBER;
+        uint8_t NUMBER;
 
         Color color;
         std::vector<uint8_t> neighbors;
 
-        // Repare que a constante NUMBER e o atributo privado 'neighbors_count' são inicializados na lista de inicialização
+        // Cria um construtor personalizado. Repare que, caso não seja passado nenhum parâmetro para o construtor da classe 'Node',
+        // o construtor padrão criará um objeto do tipo 'Node' que possui o número 255.
+        // Repare que a constante NUMBER e o atributo privado 'neighbors_count' são inicializados no array de inicialização
         // do construtor da classe 'Node', enquanto o atributo público color é inicializado dentro do construtor em questão.
-        Node(uint8_t node_number) : NUMBER(node_number), neighbors_count(0){
-            color = Configs::OFF;
-
+        Node(uint8_t node_number = 255) : NUMBER(node_number), color(Configs::OFF), neighbors_count(0){
             //Setamos n como sendo o seu próprio (e primeiro) vizinho.
             neighbors.push_back(NUMBER);
         }
@@ -88,24 +94,63 @@ class Node{
         }
 
         bool is_neighbor(uint8_t neighbor_number){
-            bool result = false;
-
             for(uint8_t i = 0; i < this->neighbors.size(); i++){
                 if(this->neighbors[i] == neighbor_number){
-                    result = true;
+                    return true;
                 }
             }
-
-            return result;
+            return false;
         }
-
 
     private:
         uint8_t neighbors_count;
 
 };
 
-//class Graph{}
+class Graph{
+    /*
+        Descrição:
+            Essa classe é responsável por implementar a abstração de um grafo. Para essa classe, consideraremos um grafo como sendo um array
+            fixa de nós.
+        Atributos:
+            public:
+                NODES_NUMBER (const uint8_t): Variável que guardará a quantidade de nós que o grafo terá, isto é, a quantidade de elementos
+                do array que representa o grafo.
+                nodes (Node[]): Array de objetos do tipo 'Node' que conterá todos os nós do grafo. 
+            private:
+                current_nodes_number (uint8_t): Variável que guardará a quantidade atual de nós que o grafo possui, isto é, a quantidade de 
+                elementos que o array que representa o grafo possui atualmente.
+        Métodos:
+            public:
+                void set_node(Node node): Método responsável por adicionar um nó ao grafo, isto é, adicionar um objeto do tipo 'Node' o array
+                que representa o grafo.
+            private:
+    */
+
+    public:
+
+        static const uint8_t NODES_NUMBER = Configs::NODES_NUMBER;
+
+        Node nodes[NODES_NUMBER];
+
+        // Cria um construtor para a classe 'Graph'.
+        Graph() : current_nodes_number(0){}
+
+        // Método responsável por adicionar um nó ao grafo.
+        void set_node(const Node& node){
+            if(current_nodes_number < NODES_NUMBER){
+                this->nodes[current_nodes_number] = node;
+                current_nodes_number++;
+            }else{
+                printf("Erro, o grafo já atingiu a sua quantidade máxima de nós.");
+            }   
+        }
+    
+    private:
+        uint8_t current_nodes_number;
+
+
+};
 
 /*              Funções que serão usadas para testar o código               */
 
@@ -117,43 +162,58 @@ void show_vector(std::vector<uint8_t> v){
     printf("]\n\n");
 }
 
-void run_tests(){
+void run_node_tests(){
     /*
-        Essa função é responsável por rodar alguns testes básicos.
+        Essa função é responsável por rodar alguns testes básicos para objetos do tipo 'Node'.
     */
 
     //Cria um objeto do tipo 'Node' para testes.
     Node n(0);
     
-    // Imprime o atributo n.NUMBER
+    // Imprime o atributo n.NUMBER.
     printf("%d\n\n", n.NUMBER);
      
-    // Imprime o atributo n.color
+    // Imprime o atributo n.color.
     printf("R: %d, G: %d, B: %d\n\n", n.color.R, n.color.G, n.color.B);
-    // Testa o método 'set_color'
+    // Testa o método 'set_color'.
     Color color_to_set = {100,0,200};
     n.set_color(color_to_set);
     printf("R: %d, G: %d, B: %d\n\n", n.color.R, n.color.G, n.color.B); 
     
     
-    // Testa a inserção de vizinhos (método 'set_neighbor')
+    // Testa a inserção de vizinhos (método 'set_neighbor').
     show_vector(n.neighbors);
     for(uint8_t i = 1; i < 10; i++){
         n.set_neighbor(i);
     }
     show_vector(n.neighbors);
 
-    // Testa o método 'is_neighbor'
+    // Testa o método 'is_neighbor'.
     printf("%d\n", n.is_neighbor(4)); // == 1.
     printf("%d\n\n", n.is_neighbor(20)); // == 0.   
 }
 
+void run_graph_tests(){
+    /*
+        Essa função é responsável por rodar alguns testes básicos para objetos do tipo 'Graph'.
+    */
+
+    //Cria um objeto do tipo 'Graph'
+    Graph G;
+    
+    // Cria 10 nós e os adiciona ao grafo G, isto é, testa o método set_node().
+    for(uint8_t i = 0; i < 10; i++){
+        Node n(i);
+        G.set_node(n);
+    }
+}
 
 /*              Código principal                */
 
 int main(){
 
-    run_tests();
+    //run_node_tests();
+    run_graph_tests();
 
     return 0;
 }
